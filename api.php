0fe9242ea6675d75fa5707a13b445e93ef9b0c2c
Disengager <disengagerAMV@gmail.com>
2018-03-25 22:36:01 +0500

    Commit 4
    Сделан реакт дом, прогрузка структуры из джсона, доработано протоапи, сделана фильтрация

--- api.php
+++ api.php
@@ -1,19 +1,44 @@
 <?php
 
-  header( 'Content-Type: application/json' );
+  switch ( $_GET['d2i4ey2'] ) {
+    case '1di6h':
+    {
 
-  $author = [  // эмитация таблицы авторы
-    ['1', 'Безруков']
-  ];
-  $categories = [ // эмитация таблицы категории
-    [ '1', 'Юмор']
-  ];
+      header( 'Content-Type: application/json' );
 
-  for($i = 0; $i < Count($author) ; $i++)
-  {
-    $data[$i] = [ 'author' => $author[$i], 'categories' => $categories[$i], 'text' => $i . '___text___' . $i, 'date' => $i . '.03.2018' ]; // построение финального массива с данными
+      $data['authors'] = [  // эмитация таблицы авторы
+        0 => ['0', 'Безруков'],
+        1 => ['1', 'Хобенский'],
+        2 => ['2', 'Пушкин'],
+        3 => ['3', 'Достоевский'],
+      ];
+      $data['categories'] = [ // эмитация таблицы категории
+        0 => [ '0', 'Юмор'],
+        1 => [ '1', 'Жизнь'],
+        2 => [ '2', 'Война'],
+        3 => [ '3', 'Зрелость'],
+      ];
+
+      //for($i = 0; $i < Count(  $data['authors']) ; $i++)
+      for($i = 0; $i < 2 ; $i++)
+      {
+        $data['quotes'][$i] = [ 'author' => $data['authors'][$i][0], 'categories' => $data['categories'][$i][0], 'text' => $i . '___text___' . $i, 'date' => $i . '.03.2018' ]; // построение финального массива с данными
+      }
+
+      echo json_encode( $data );
+
+    }
+    break;
+    case '12kd8':
+    {
+
+    }
+    default:
+    {
+
+    }
+      break;
   }
 
-  echo json_encode( $data );
 
 ?>
--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,145 @@
-'use strict'
+'use strict';
+
+// формат фильтра authorsFilter = { '1':''  }, categoriesFilter = { '2': '' }
+var authorsFilter = {   },
+    categoriesFilter = {  },
+
+// блок для фильтрации
+    filterBlock = React.createClass({
+    displayName: 'filterBlock',
+    render: function render() {
+
+      var itemsAuthors, itemsCategories;
+
+      //передаём свойство со списком авторов и категорий, чтобы выстроить фильтры
+
+      if (this.props.authors.length != 0)
+        itemsAuthors = this.props.authors.map(function (item) {
+          return React.createElement( 'div', {},
+            React.createElement( 'input', { type: 'checkbox', name: '',  className: '', id: 'author-filter-form__checkbox__' + item[0] } ),
+            React.createElement( 'label', {  className: '', htmlFor: 'author-filter-form__checkbox__' + item[0] }, item[1] ),
+          );
+        });
+      if (this.props.categories.length != 0)
+        itemsCategories = this.props.categories.map(function (item) {
+          return React.createElement( 'div', {},
+            React.createElement( 'input', { type: 'checkbox', name: '',  className: '', id: 'categories-filter-form__checkbox__' + item[0] } ),
+            React.createElement( 'label', {  className: '', htmlFor: 'categories-filter-form__checkbox__' + item[0] }, item[1]  ),
+          )
+        });
+
+      return React.createElement( 'div', { className: 'main-page__filter-block' },
+              React.createElement( 'div', { className: 'main-page__filter-block title' }, 'Фильтер' ),
+              React.createElement( 'div', { className: 'main-page__filter-block filter-form' },
+                React.createElement( 'div', { className: 'main-page__filter-block filter-form__title' }, 'Выберите автора' ),
+                React.createElement( 'form', { className: 'main-page__filter-block filter-form__title js-hiden' },
+                  React.createElement( 'div', {},
+                    // React.createElement( 'input', { type: 'checkbox', name: '',  className: '', id: 'author-filter-form__checkbox__-1' } ),
+                    // React.createElement( 'label', {  className: '', htmlFor: 'author-filter-form__checkbox__-1' }, 'все' ),
+                  ),
+                  itemsAuthors
+                )
+              ),
+              React.createElement( 'div', { className: 'main-page__filter-block filter-form' },
+                React.createElement( 'div', { className: 'main-page__filter-block filter-form__title' }, 'Выберите категорию' ),
+                React.createElement( 'form', { className: 'main-page__filter-block filter-form__title js-hiden' },
+                  React.createElement( 'div', {},
+                    // React.createElement( 'input', { type: 'checkbox', name: '',  className: '', id: 'categories-filter-form__checkbox__-1' } ),
+                    // React.createElement( 'label', {  className: '', htmlFor: 'categories-filter-form__checkbox__-1' }, 'все' ),
+                  ),
+                  itemsCategories
+                )
+              ),
+              React.createElement( 'div', { className: 'main-page__filter-block button' }, 'отфильтрить' )
+      )
+    }
+}),
+    quotesBlock = React.createClass({
+    displayName: 'QuotesBlock',
+    render: function render() {
+
+      var itemsQuotes, _this = this;
+
+      if (this.props.quotes.length != 0)
+        itemsQuotes = this.props.quotes.map(function (item) {
+
+          if( authorsFilter[ item['author'] ] != undefined || $.isEmptyObject(authorsFilter) )
+            if( categoriesFilter[ item['categories'] ] != undefined || $.isEmptyObject(categoriesFilter) )
+              return React.createElement( 'div', { className: 'main-page__quotes-block__quote'},
+                      React.createElement( 'div', { className: 'main-page__quotes-block__quote__string'},
+                        React.createElement( 'div', { className: 'main-page__quotes-block__quote__string__buttons'},
+                          React.createElement( 'span', { className: 'main-page__quotes-block__quote__string__buttons' }, 'изменить' ),
+                          React.createElement( 'span', { className: 'main-page__quotes-block__quote__string__buttons' }, 'закрыть' )
+                        )
+                      ),
+                      React.createElement( 'div', { className: 'main-page__quotes-block__quote__string'},
+                        React.createElement( 'div', { className: 'main-page__quotes-block__quote__string caption'}, 'Автор' ),
+                        React.createElement( 'div', { className: 'main-page__quotes-block__quote__string value'},  _this.props.authors[ item['author'] ] ? _this.props.authors[ item['author'] ][1] :'' ),
+                      ),
+                      React.createElement( 'div', { className: 'main-page__quotes-block__quote__string'},
+                        React.createElement( 'div', { className: 'main-page__quotes-block__quote__string caption'}, 'Цитата' ),
+                        React.createElement( 'div', { className: 'main-page__quotes-block__quote__string value'}, item['text'] ),
+                      ),
+                      React.createElement( 'div', { className: 'main-page__quotes-block__quote__string'},
+                        React.createElement( 'div', { className: 'main-page__quotes-block__quote__string caption'}, 'Дата' ),
+                        React.createElement( 'div', { className: 'main-page__quotes-block__quote__string value'}, item['date'] ),
+                      ),
+                      React.createElement( 'div', { className: 'main-page__quotes-block__quote__string'},
+                        React.createElement( 'div', { className: 'main-page__quotes-block__quote__string caption'}, 'Категория' ),
+                        React.createElement( 'div', { className: 'main-page__quotes-block__quote__string value'},  _this.props.categories[ item['categories'] ] ? _this.props.categories[ item['categories'] ][1] :''  ),
+                      )
+                   )
+              else
+                  return;
+        });
+
+        return React.createElement('div', { className: 'main-page__quotes-block'},
+                itemsQuotes
+        )
+      }}),
+    Wrapper = React.createClass({
+    displayName: 'Wrapper',
+    setStatFunction: function setStatFunction(e) {
+
+    },
+    loadCommentsFromServer: function loadCommentsFromServer() {
+      // alert( this.props.url );
+      // console.log( this.state.authors );
+    },
+    componentDidMount: function componentDidMount() {
+      // this.loadCommentsFromServer();
+    },
+    getInitialState: function getInitialState() {
+
+      var url = this.props.url,
+          v,
+          _this = this;
+
+      jQuery.get( url, function(data) {
+
+        v = data;
+
+      }).done(function() {
+
+        _this.setState({ quotes: v.quotes });
+        _this.setState({ authors: v.authors });
+        _this.setState({ categories: v.categories });
+
+        _this.loadCommentsFromServer();
+
+      });
+
+
+      return { quotes: [], authors: [], categories: [] };
+    },
+    render: function render() {
+      return React.createElement(
+        'div',
+        { className: 'main-page__wrapper' },
+        React.createElement( filterBlock, { categories: this.state.categories, authors: this.state.authors } ),
+        React.createElement( quotesBlock, { categories: this.state.categories, authors: this.state.authors, quotes: this.state.quotes } ),
+      );
+    }
+});
+
+ReactDOM.render(React.createElement(Wrapper, { url: 'api.php?d2i4ey2=1di6h' }), document.querySelector('.main-page'));
