c97e067b3ea5ba5e61519645f716a9ea109959f3
Disengager <disengagerAMV@gmail.com>
2018-03-25 18:30:01 +0500

    Commit 2
    Основная структура реакта, ориентеровочное апи

--- api.php
+++ api.php
@@ -1,4 +1,19 @@
 <?php
 
+  header( 'Content-Type: application/json' );
+
+  $author = [  // эмитация таблицы авторы
+    ['1', 'Безруков']
+  ];
+  $categories = [ // эмитация таблицы категории
+    [ '1', 'Юмор']
+  ];
+
+  for($i = 0; $i < Count($author) ; $i++)
+  {
+    $data[$i] = [ 'author' => $author[$i], 'categories' => $categories[$i], 'text' => $i . '___text___' . $i, 'date' => $i . '.03.2018' ]; // построение финального массива с данными
+  }
+
+  echo json_encode( $data );
 
 ?>
--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-'use strict'
+'use strict';
+
+var Wrapper = React.createClass({
+  displayName: 'Wrapper',
+
+  loadCommentsFromServer: function loadCommentsFromServer() {
+
+  },
+  componentDidMount: function componentDidMount() {
+    this.loadCommentsFromServer();
+  },
+  render: function render() {
+    return React.createElement(
+      'div',
+      { className: 'main-page__wrapper' }
+    );
+  }
+});
+
+alert( 1+1 );
+ReactDOM.render(React.createElement(Wrapper, { url: 'api.php' }), document.querySelector('.main-page'));
